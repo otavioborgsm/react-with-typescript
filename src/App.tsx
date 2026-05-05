23ceed4ff804f196a5ce0ffaad40e9de7e08b24d
otavioborgsm <otavio.demb@rede.ulbra.br>
2026-05-05 10:30:17 -0300

    0201: test TypeScript type inference

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 function App() {
-  return <div>React</div>;
+  const [total, setTotal] = React.useState(0);
+
+  function incrementar() {
+    setTotal((total) => total + 1);
+  }
+
+  return (
+    <div>
+      <p>Total: {total}</p>
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  );
 }
 
 export default App;
